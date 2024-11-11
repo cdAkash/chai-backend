80a5ef1f356730abef4cd42a3e5eb1e0e5ef2c58
cdAkash <akashyadav15032002@gmail.com>
2024-11-11 19:02:15 +0530

    Till Api ERROR and RESPONSE

---
+++ src/index.js
@@ -0,0 +1,37 @@
+/*
+
+import mongoose from "mongoose";
+import {DB_NAME} from "./constants"
+import express from "express"
+const app = express()
+;(async ()=>{
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("error",(error)=>{
+            console.log("Errr:",error)
+            throw error
+        })
+
+        app.listen(process.env.PORT,()=>{
+            console.log(`App is listening on port ${process.env.PORT}`)
+        })
+    } catch (error) {
+        console.error(error)
+    }
+})() // IFFE method calling
+
+*/
+// require('dotenv').config({path:'./env'})
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path:'./env'
+})
+connectDB().then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server is running at Port: ${process.env.PORT}`)
+    })
+}).catch((err)=>{
+    console.log("mongoDB connection Failed->", err)
+})
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,25 @@
+const asyncHandler =(requestHandler)=>{
+    (req,res,next)=>{
+        Promise.resolve(requestHandler(req,res,next)).reject((err)=>next(err))
+    }
+}
+
+export {asyncHandler}
+
+
+
+// const asyncHandler = ()=>{} //Normal callback function
+// const asyncHandler =(func)=>{ ()=>{} } // passing a function as argument to another function
+// const asyncHandler =(func)=>{ async ()=>{} } 
+// const asyncHandler =(func)=> ()=>{}  // we can remove the outer curly braces
+
+// const asyncHandler =(func)=>{ async(req,res,next)=>{
+//     try {
+//         await func(req,res,next)
+//     } catch (error) {
+//         res.status(error.code || 500).json({
+//             success:false,
+//             message: error.message
+//         })
+//     }
+// } }
